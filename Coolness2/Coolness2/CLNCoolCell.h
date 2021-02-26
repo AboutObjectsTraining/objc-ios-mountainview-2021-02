1808164f7bcd8029876412b2ea60f8bda88601b5
Jonathan Lehr <jonathan@aboutobjects.com>
2021-02-25 19:25:52 -0500

    Added Coolness2

---
+++ Coolness2/Coolness2/CLNCoolCell.h
@@ -0,0 +1,13 @@
+// Copyright (C) 2021 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+#import <UIKit/UIKit.h>
+
+extern const UIEdgeInsets CLNTextInsets;
+
+@interface CLNCoolCell : UIView
+
+@property (copy, nonatomic) NSString *text;
+
+@end
+
---
+++ Coolness2/Coolness2/CLNCoolCell.m
@@ -0,0 +1,125 @@
+// Copyright (C) 2021 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+#import "CLNCoolCell.h"
+
+const UIEdgeInsets CLNTextInsets = {
+    .top = 7,
+    .bottom = 8,
+    .left = 12,
+    .right = 12
+};
+
+@interface CLNCoolCell ()
+@property (getter=isHighlighted, nonatomic) BOOL highlighted;
+@property (class, readonly, nonatomic) NSDictionary *textAttributes;
+@end
+
+@implementation CLNCoolCell
+
+// FIXME: Cover the initWithCoder: path.
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self == nil) return nil;
+
+    [self configureAppearance];
+    [self configureGestureRecognizers];
+    
+    return self;
+}
+
+- (void)configureAppearance {
+    self.layer.borderWidth = 3;
+    self.layer.borderColor = UIColor.whiteColor.CGColor;
+    self.layer.masksToBounds = YES;
+    self.layer.cornerRadius = 10;
+    self.backgroundColor = UIColor.systemBlueColor;
+}
+
+- (void)configureGestureRecognizers {
+    UITapGestureRecognizer *recognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(bounce)];
+    recognizer.numberOfTapsRequired = 2;
+    [self addGestureRecognizer:recognizer];
+}
+
+
++ (NSDictionary *)textAttributes {
+    // TODO: cache an instance
+    return @{ NSFontAttributeName : [UIFont boldSystemFontOfSize:20],
+              NSForegroundColorAttributeName : UIColor.whiteColor };
+}
+
+- (void)setHighlighted:(BOOL)highlighted {
+    _highlighted = highlighted;
+    self.alpha = highlighted ? 0.5 : 1.0;
+}
+
+- (void)setText:(NSString *)text {
+    _text = [text copy];
+    [self sizeToFit];
+}
+
+// MARK: - Animation
+
+- (void)bounce {
+    NSLog(@"In %s", __func__);
+    [self animateWithDuration:1 size:CGSizeMake(120, 240)];
+}
+
+- (void)configureAnimationWithSize:(CGSize)size {
+    [UIView modifyAnimationsWithRepeatCount:5 autoreverses:YES animations:^{
+        CGAffineTransform translation = CGAffineTransformMakeTranslation(size.width, size.height);
+        self.transform = CGAffineTransformRotate(translation, M_PI_2);
+    }];
+}
+
+- (void)animateWithDuration:(NSTimeInterval)duration size:(CGSize)size {
+    [UIView animateWithDuration:duration
+                     animations:^{ [self configureAnimationWithSize:size]; }
+                     completion:^(BOOL finished) { self.transform = CGAffineTransformIdentity; }];
+}
+
+
+// MARK: - Drawing and resizing
+
+- (CGSize)sizeThatFits:(CGSize)size {
+    CGSize newSize = [self.text sizeWithAttributes:self.class.textAttributes];
+    newSize.width += CLNTextInsets.left + CLNTextInsets.right;
+    newSize.height += CLNTextInsets.top + CLNTextInsets.bottom;
+    return newSize;
+}
+
+- (void)drawRect:(CGRect)rect {
+    CGPoint origin = CGPointMake(CLNTextInsets.left, CLNTextInsets.top);
+    [self.text drawAtPoint:origin withAttributes:self.class.textAttributes];
+}
+
+
+// MARK: - UIResponder methods
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self.superview bringSubviewToFront:self];
+    self.highlighted = YES;
+}
+
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = touches.anyObject;
+    CGPoint currLocation = [touch locationInView:nil];
+    CGPoint prevLocation = [touch previousLocationInView:nil];
+    
+    CGFloat deltaX = currLocation.x - prevLocation.x;
+    CGFloat deltaY = currLocation.y - prevLocation.y;
+    
+    self.frame = CGRectOffset(self.frame, deltaX, deltaY);
+}
+
+- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    self.highlighted = NO;
+}
+- (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    self.highlighted = NO;
+}
+
+@end
